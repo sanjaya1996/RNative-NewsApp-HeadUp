a3117c044365a598d9cb72f2813017180457772e
Sanzay Dahal <sanzaydahal12345@gmail.com>
2021-05-23 13:12:21 +1000

    Initial Setup

---
+++ App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View, useColorScheme, StatusBar} from 'react-native';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View style={styles.screen}>
+        <Text>This is a News App</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
